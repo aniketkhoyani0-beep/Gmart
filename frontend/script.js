0a753e00f977a2f13c1f5b73ab60ba3f2e09161a
aniketkhoyani0-beep <aniketkhoyani0@gmail.com>
2025-12-04 18:41:50 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,11 @@
 document.getElementById('testBtn').addEventListener('click', async () => {
     try {
-        const response = await fetch('https://gmart-backend-7kyz.onrender.com/test');
+        const response = await fetch('https://gmart-backend-7kyz.onrender.com/api/test');
+
         const data = await response.json();
         document.getElementById('result').textContent = data.message;
     } catch (err) {
         document.getElementById('result').textContent = 'Error connecting to API';
     }
 });
+
